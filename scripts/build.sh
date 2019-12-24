ec717bc3897036bb561df7f7eaa7b3451da7069f
kstasi <nastasiakondaurova@gmail.com>
2019-12-24 11:19:19 +0200

    math building command added

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,7 @@ mkdir -p build/
 ligo compile-contract ./contracts/SimpleCoin.ligo main > ./build/SimpleCoin_tmp.tz 
 tr -d '\r' < ./build/SimpleCoin_tmp.tz > ./build/SimpleCoin.tz
 rm ./build/*_tmp.tz *.pp.ligo
+
+ligo compile-contract ./contracts/Math.ligo main > ./build/Math_tmp.tz 
+tr -d '\r' < ./build/Math_tmp.tz > ./build/Math.tz
+rm ./build/*_tmp.tz *.pp.ligo
